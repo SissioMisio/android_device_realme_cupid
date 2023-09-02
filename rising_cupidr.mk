12879c5285e098421fd361d861b0340f0c400b78
SissioMisio <40896232+SissioMisio@users.noreply.github.com>
2023-09-02 20:11:50 +0200

    correction

--- rising_cupidr.mk
+++ rising_cupidr.mk
@@ -12,7 +12,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/realme/cupidr/device.mk)
 
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/rising/config/common_full_phone.mk)
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 PRODUCT_NAME := rising_cupidr
 PRODUCT_DEVICE := cupidr
